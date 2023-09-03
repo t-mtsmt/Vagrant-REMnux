04424ea33c8ce61acd6f4a2fca7d834180ce65a6
t-mtsmt <tmtsmt.sec@gmail.com>
2023-09-03 21:40:20 +0900

    Fixed remnux-cli version

--- provisioners/remnux-install.sh
+++ provisioners/remnux-install.sh
@@ -4,10 +4,13 @@
 apt update
 apt upgrade -y
 
-# install remnux
-wget https://REMnux.org/remnux-cli -O /usr/local/bin/remnux
-chmod +x /usr/local/bin/remnux
+# install remnux-cli
+curl -Lo remnux-cli-linux https://github.com/REMnux/remnux-cli/releases/download/v1.3.4/remnux-cli-linux
+chmod +x remnux-cli-linux
+mv remnux-cli-linux /usr/local/bin/remnux 
 apt install -y gnupg
+
+# install remnux
 /usr/local/bin/remnux install
 
 # enable ssh service for vagrant
